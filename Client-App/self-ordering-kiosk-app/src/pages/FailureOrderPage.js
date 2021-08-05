7415a60bb9783ec400fb6d6fef3af641f3fbb9af
NikoLa437 <nikoladskv@hotmail.rs>
2021-08-05 23:02:07 +0200

    Add failure page and modify timer for redirect page

--- Client-App/self-ordering-kiosk-app/src/pages/FailureOrderPage.js
+++ Client-App/self-ordering-kiosk-app/src/pages/FailureOrderPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect} from "react";
 
 
-const FinishedOrderPage = () => {
+const FailureOrderPage = () => {
 
     useEffect(() => {
         setTimeout(() => {
@@ -14,16 +14,16 @@ const FinishedOrderPage = () => {
 			<div className="home-page-background align-items-center">
 				<div className="col-md-12 p-0 bg-indigo h-md-100">
 					<div className="row text-white d-md-flex align-items-center h-25 p-5 text-center justify-content-center">
-						<h1 className="display-2 w-100">Dear Customer,</h1>
-						<h1 className="display-2 ">Your order created successfully</h1>
 					</div>
-					<div className="text-white d-md-flex align-items-center h-25 p-5 text-center justify-content-center">
-						<h1 style={{'fontSize':'70px'}} className="align-items-top display-1">Please take your account below</h1>
+                    <div className="row text-white d-md-flex align-items-center h-25 p-5 text-center justify-content-center">
+						<h1 className="display-1 w-100">Dear Customer,</h1>
+						<h1 className="display-1 ">We have some problem, please contact waiters</h1>
 					</div>
+
 				</div>
 			</div>
 		</React.Fragment>
 	);
 };
 
-export default FinishedOrderPage;
+export default FailureOrderPage;
