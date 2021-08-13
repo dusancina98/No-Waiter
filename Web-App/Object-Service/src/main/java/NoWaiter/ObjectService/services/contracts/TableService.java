4ab804a9f89f04c2a0f0a13c30602202889acf98
NikoLa437 <nikoladskv@hotmail.rs>
2021-08-13 18:16:56 +0200

    Implement qr code generator for table in object

--- Web-App/Object-Service/src/main/java/NoWaiter/ObjectService/services/contracts/TableService.java
+++ Web-App/Object-Service/src/main/java/NoWaiter/ObjectService/services/contracts/TableService.java
@@ -1,15 +1,20 @@
 package NoWaiter.ObjectService.services.contracts;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.UUID;
 
 import javax.security.auth.message.AuthException;
+
+import com.google.zxing.WriterException;
 
 import NoWaiter.ObjectService.services.contracts.dto.IdentifiableDTO;
 import NoWaiter.ObjectService.services.contracts.dto.TableDTO;
 
 public interface TableService {
 
-	IdentifiableDTO<TableDTO> createTable(UUID objectAdminId);
+	IdentifiableDTO<TableDTO> createTable(UUID objectAdminId) throws Exception;
 		
 	Iterable<IdentifiableDTO<TableDTO>> findAllForObjectById(UUID objectId);
 	
---
+++ Web-App/Object-Service/src/main/java/NoWaiter/ObjectService/services/implementation/util/QrCodeGenerator.java
@@ -0,0 +1,79 @@
+package NoWaiter.ObjectService.services.implementation.util;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import org.json.JSONObject;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class QrCodeGenerator {
+	
+	public QrCodeGenerator() {
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public byte[] generateQrCode(String objectId, String tableId) throws Exception {
+		String filePath = "QRCode.png";
+		String charset = "UTF-8";
+		@SuppressWarnings("rawtypes")
+		Map hintMap = new HashMap();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		System.out.println("TEST1");
+		Map<String, String> qrCodeDataMap = Map.of(
+		"ObjectId", objectId,
+		"TableId", tableId,
+		"Key", "123" 
+		// see next section for ´generateVerificationKey´ method
+		);
+
+		String jsonString = new JSONObject(qrCodeDataMap).toString();
+		createQRCode(jsonString, filePath, charset, hintMap, 500, 500);
+
+		BufferedImage image = ImageIO.read(new File(filePath));
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+		ImageIO.write(image, "png", baos);
+
+		byte[] imageData = baos.toByteArray();
+		
+		return imageData;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private void createQRCode(String qrCodeData, 
+            String filePath, 
+            String charset, 
+            Map hintMap, 
+            int qrCodeHeight, 
+            int qrCodeWidth) throws Exception {
+
+		BitMatrix matrix = new MultiFormatWriter().encode(
+		new String(qrCodeData.getBytes(charset), charset),
+		BarcodeFormat.QR_CODE,
+			qrCodeWidth,
+			qrCodeHeight,
+			hintMap
+			);
+			
+		MatrixToImageWriter.writeToPath(
+		matrix,
+		filePath.substring(filePath.lastIndexOf('.') + 1),
+		FileSystems.getDefault().getPath(filePath)
+			);
+	}
+
+}
